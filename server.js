a1bd81fc27fed3adf805d29344138abb46e95ce6
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-07-30 18:41:42 -0300

    install nunjucks

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 
 const server = express()
+
+server.set("view engine","html")
+
+server.use(express.static('public'))
+server.use(express.static('src'))
+
+nunjucks.configure("views", {
+    express: server,
+    noCache: true
+})
 
 server.listen(5000, function() {
     console.log('Server is running')
 })
 
 server.get("/", function(req, res) {
-    return res.send("Nodemon !!")
+    return res.render("index")
 })
