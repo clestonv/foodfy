5cef84a1e771fcb4ff8d034cb8ba75dc7bbd8307
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-10-07 00:27:21 -0300

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const PORT = process.env.PORT || 5000
 const server = express()
 const recipes = require("./data")
+const PORT = process.env.PORT || 5000
 
 server.set("view engine","html")
 
