152025ca124fea8dad14e302ed14a4df47187154
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-12-27 18:48:21 -0300

    refatorado rotas

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,11 +1,33 @@
-const recipes = require('../data.json')
+const data = require('../data.json')
 
 //home 
 exports.home = function (req, res ) {
     let recipesFiltered = []
     for(let i = 0; i < 6; i++) {
-        recipesFiltered.push(recipes[i])
+        recipesFiltered.push(data.recipes[i])
     }
 
     return res.render("home", { items: recipesFiltered })
 }
+
+exports.about = function(req, res) {
+    return res.render("about")
+}
+
+exports.recipes = function(req, res) {
+    let recipes = []
+    for( let i = 0; i < data.recipes.length; i++ ) {
+       const obj = data.recipes[i]
+       obj.index = i
+    }
+
+    return res.render("recipe", { items: data.recipes })
+}
+
+exports.food = function (req, res) {
+    const { index: recipeIndex } = req.params
+
+    const recipe = data.recipes[recipeIndex]
+
+    return res.render("recipes", { item: recipe})
+  }
--- routes.js
+++ routes.js
@@ -4,32 +4,10 @@ const recipes = require('./controllers/recipes')
 
 routes.get('/', recipes.home )
 
-routes.get("/about", function(req, res) {
-    return res.render("about")
-})
+routes.get("/about", recipes.about )
 
-routes.get("/recipe", function(req, res) {
+routes.get("/recipe", recipes.recipes )
 
-    let recipesFiltered = []
-
-    for( let i = 0; i < recipes.length; i++ ) {
-       const obj = recipes[i]
-       obj.index = i
-   
-    }
-
-
-    return res.render("recipe", { items: recipes })
-})
-
-routes.get("/recipes/:index", function (req, res) {
-    const { index: recipeIndex } = req.params
-
-    const recipe = recipes[recipeIndex]
-
-    return res.render("recipes", { item: recipe})
-  })
-
-routes.get('/cool', (req, res) => res.send(cool()))
+routes.get("/recipes/:index", recipes.food )
 
 module.exports = routes
