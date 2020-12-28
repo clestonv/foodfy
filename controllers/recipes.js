2fd599f52669f9710d4210c6eccf7fe961330774
Cleberson Osorio <cleberson.osorioti@hotmail.com>
2020-12-27 21:17:59 -0300

    Criando rotas do painel admin

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,6 +1,8 @@
 const data = require('../data.json')
 
-//home 
+/**
+ * Páginas Web
+ */
 exports.home = function (req, res ) {
     let recipesFiltered = []
     for(let i = 0; i < 6; i++) {
@@ -9,11 +11,9 @@ exports.home = function (req, res ) {
 
     return res.render("home", { items: recipesFiltered })
 }
-
 exports.about = function(req, res) {
     return res.render("about")
 }
-
 exports.recipes = function(req, res) {
     let recipes = []
     for( let i = 0; i < data.recipes.length; i++ ) {
@@ -23,11 +23,25 @@ exports.recipes = function(req, res) {
 
     return res.render("recipe", { items: data.recipes })
 }
-
 exports.food = function (req, res) {
     const { index: recipeIndex } = req.params
 
     const recipe = data.recipes[recipeIndex]
 
     return res.render("recipes", { item: recipe})
-  }
+}
+/**
+ * Páginas Admin
+ */
+exports.index = function (req, res) {
+    return res.render("index")
+}
+exports.create = function (req, res) {
+    return res.send('Create')
+}
+exports.show = function (req, res) {
+    return res.send('Show')
+}
+exports.edit = function (req, res) {
+    return res.send('Edit')
+}
--- routes.js
+++ routes.js
@@ -3,11 +3,14 @@ const routes = express.Router()
 const recipes = require('./controllers/recipes')
 
 routes.get('/', recipes.home )
-
 routes.get("/about", recipes.about )
-
 routes.get("/recipe", recipes.recipes )
-
 routes.get("/recipes/:index", recipes.food )
+
+
+routes.get("/admin/recipes", recipes.index )
+routes.get("/admin/recipes/create", recipes.create )
+routes.get("/admin/recipes/:id", recipes.show )
+routes.get("/admin/recipes/:id/edit", recipes.edit )
 
 module.exports = routes
